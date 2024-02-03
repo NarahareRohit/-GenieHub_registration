3c8c5f62c540d62d433a2e43a2fb57abc094f8fd
Rohit Narahare <71591573+NarahareRohit@users.noreply.github.com>
2024-02-03 18:54:18 +0530

    Delete LoginRegister directory

--- LoginRegister/GenieHub/GenieHub/src/main/java/com/app/GenieHub/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.GenieHub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.GenieHub.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
-
-}
